00d0863f7d394568b3297b6f3d3629c2c906f5a3
Victor Kiptoo <kimutaiv345@gmail.com>
2026-04-20 14:09:41 +0300

    class example

---
+++ 00_python_basics/02_data_types.py
@@ -0,0 +1,10 @@
+name1 = "Victor"
+name2 = "Kiptoo"
+age = 157
+boy = True
+balance = 1799.99
+
+print(id(name1))
+print(id(name2))
+
+
---
+++ 00_python_basics/03_control_flow.py
@@ -0,0 +1,6 @@
+num = 50
+
+if (num/5)>10:
+    print ("True")
+else:
+    print("False")
